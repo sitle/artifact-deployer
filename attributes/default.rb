7c5f109c404be7b0312be672e30506456d30fd91
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-04-10 22:31:48 +0200

    using domain as node attribute in jvm_host recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['jvm_host']['tomcat_defaults_path'] = "/etc/default/tomcat7"
 
 default['jvm_host']['add_host_param'] = false
 default['jvm_host']['param_name'] = "host"
-default['jvm_host']['hostname'] = "#{node[:hostname]}.#{node[:resolver][:search]}"
+default['jvm_host']['hostname'] = "#{node[:hostname]}.#{node[:domain]}"
