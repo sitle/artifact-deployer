38ccfd2b5e4e449da332ad9802ddff0d0b8beb34
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-04-10 19:12:44 +0200

    added chef info message

--- recipes/jvm_host.rb
+++ recipes/jvm_host.rb
@@ -8,6 +8,7 @@ if node.attribute?("jvm_host")
   if add_host_param
     ruby_block "Add JVM params to Tomcat" do
       block do
+        Chef::Log.info("Patching file '#{tomcat_defaults_path}' with additional JAVA_OPTS #{paramName}=#{hostname}")
         fe = Chef::Util::FileEdit.new(tomcat_defaults_path)
         jvm_opts = "-D#{param_name}=#{hostname}"
         fe.insert_line_if_no_match(/Dmanaged_by_chef/, "JAVA_OPTS=\"${JAVA_OPTS} #{jvm_opts}\" ##managed_by_chef")
