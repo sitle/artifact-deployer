9cd835e06118042eaebbaadec87e0a32cd6ea701
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-05-15 17:13:34 +0200

    added support for adding/replacing properties using attribute filtering_mode

--- recipes/artifacts.rb
+++ recipes/artifacts.rb
@@ -14,55 +14,80 @@ end
 
 node[:artifacts].each do |artifactName, artifact|
 
+  artifact_id   = artifact[:artifactId]
+  group_id      = artifact[:groupId]
+  version       = artifact[:version]
   artifactType  = artifact[:type] ? artifact[:type] : "jar"
   owner         = artifact[:owner] ? artifact[:owner] : "root"
   unzip         = artifact[:unzip] ? artifact[:unzip] : false
   classifier    = artifact[:classifier] ? artifact[:classifier] : ""
   destination   = artifact[:destination]
-  enabled       = artifact[:enabled]
-  properties    = artifact[:properties]
+  enabled       = artifact[:enabled] ? artifact[:enabled] : false
+  properties    = artifact[:properties] ? artifact[:properties] : []
+  terms         = artifact[:terms] ? artifact[:terms] : []
+  filtering_mode = artifact[:filtering_mode] ? artifact[:filtering_mode] : "replace"
 
   if enabled == true
-    maven "#{artifactName}" do
-      artifact_id   artifact[:artifactId]
-      group_id      artifact[:groupId]
-      version       artifact[:version]
-      if classifier != ''
-        classifier  classifier
+    if artifact_id and group_id and version
+      maven "#{artifactName}" do
+        artifact_id   artifact_id
+        group_id      group_id
+        version       version
+        if classifier != ''
+          classifier  classifier
+        end
+        action        :put
+        dest          chef_cache
+        owner         owner
+        packaging     artifactType
+        repositories  repositories
       end
-      action        :put
-      dest          chef_cache
-      owner         owner
-      packaging     artifactType
-      repositories  repositories
+
+      directory "fix-permissions-on-destination-folder-for-#{artifactName}" do
+        path    destination
+        owner   owner
+        action  :create
+        subscribes  :create, "maven[#{artifactName}]"
+      end
+
+      if unzip == true
+        execute "unzipping_package-#{artifactName}" do
+          command     "unzip -q -u -o  #{chef_cache}/#{artifactName}.#{artifactType} -d #{destination}/#{artifactName}; chown -R #{owner} #{destination}/#{artifactName}; chmod -R 755 #{destination}/#{artifactName}"
+          user        owner
+        end
+      else
+        execute "unzipping_package-#{artifactName}" do
+          command     "cp -Rf #{chef_cache}/#{artifactName}.#{artifactType} #{destination}/#{artifactName}.#{artifactType}; chown -R #{owner} #{destination}/#{artifactName}.#{artifactType}"
+          user        owner
+        end
+      end
     end
 
-    directory "fix-permissions-on-destination-folder-for-#{artifactName}" do
-      path    destination
-      owner   owner
-      action  :create
-      subscribes  :create, "maven[#{artifactName}]"
-    end
-
-    if unzip == true
-      execute "unzipping_package-#{artifactName}" do
-        command     "unzip -q -u -o  #{chef_cache}/#{artifactName}.#{artifactType} -d #{destination}/#{artifactName}; chown -R #{owner} #{destination}/#{artifactName}; chmod -R 755 #{destination}/#{artifactName}"
-        user        owner
-      end
-      
-      properties.each do |fileToPatch, propertyMap|
+    properties.each do |fileToPatch, propertyMap|
+      filtering_mode = propertyMap[:filtering_mode] ? propertyMap[:filtering_mode] : filtering_mode
+      if filtering_mode == "replace"
         propertyMap.each do |propName, propValue|
           file_replace_line "#{destination}/#{artifactName}/#{fileToPatch}" do
             replace "#{propName}="
             with    "#{propName}=#{propValue}"
+            only_if { File.exist?("#{destination}/#{artifactName}/#{fileToPatch}") }
+          end
+        end
+      elsif filtering_mode == "append"
+        propertyMap.each do |propName, propValue|
+          file_append "#{destination}/#{artifactName}/#{fileToPatch}" do
+            line    "#{propName}=#{propValue}"
           end
         end
       end
-        
-    else
-      execute "unzipping_package-#{artifactName}" do
-        command     "cp -Rf #{chef_cache}/#{artifactName}.#{artifactType} #{destination}/#{artifactName}.#{artifactType}; chown -R #{owner} #{destination}/#{artifactName}.#{artifactType}"
-        user        owner
+    end
+
+    terms.each do |fileToPatch, termMap|
+      termMap.each do |termMatch, termReplacement|
+        file_replace "#{destination}/#{artifactName}/#{fileToPatch}" do
+          replace "@@#{termMatch}@@"
+          with    "#{termReplacement}"
+        end
       end
     end
   end
