606df091301fd4d4295959402a5aab063c60f88d
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-05-19 21:46:22 +0200

    new version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "maurizio@session.it"
 license           "Apache 2.0"
 description       "A wrapper of the chef (Apache) Maven recipe that makes dependency fetching and unpacking dead easy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.1"
+version           "0.5.2"
 
 depends "maven"
 depends "file"
