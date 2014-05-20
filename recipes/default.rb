87ca4aa9dc62e18be2310a70f6ec5af3acc9b3af
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-05-20 20:17:32 +0200

    added encryption for mvn passwords

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe "maven::default"
 
-m2_home = node['maven']['m2_home']
+m2_home         = node['maven']['m2_home']
+master_password = node['maven']['master_password']
 
 template  "#{m2_home}/conf/settings.xml" do
   source  "settings.xml.erb"
@@ -10,12 +11,25 @@ template  "#{m2_home}/conf/settings.xml" do
   variables(
     :repos => MavenReposCookbook.repos
   )
-  subscribes :create, "ark[maven]", :immediately
+end
+
+unless master_password.nil? and master_password.empty?
+  directory  "/root/.m2" do
+    mode    0666
+    owner   "root"
+    group   "root"
+  end
+
+  template  "/root/.m2/settings-security.xml" do
+    source  "settings-security.xml.erb"
+    mode    0666
+    owner   "root"
+    group   "root"
+  end
 end
 
 link "/usr/bin/mvn" do
   to "/usr/local/maven/bin/mvn"
-  subscribes :create, "template[#{m2_home}/conf/settings.xml]", :immediately
 end
 
 include_recipe "artifact-deployer::artifacts"
